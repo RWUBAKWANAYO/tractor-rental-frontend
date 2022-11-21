c698bd213753606e7cdb27d8e6e6f93778a8bbe3
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2022-11-21 23:05:12 +0200

    impliment popular tractors in home

---
+++ src/features/Tractors/services/PopularTractorsSlice.js
@@ -0,0 +1,38 @@
+/* eslint-disable no-param-reassign */
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import AxiosInstance from '../../../lib/AxiosInstance';
+
+const initialState = {
+  status: null,
+  loading: false,
+  data: [],
+  error: '',
+};
+
+export const FetchPopularTractors = createAsyncThunk('populartractors', async (_, { rejectWithValue }) => {
+  try {
+    const res = await AxiosInstance({ url: 'api/v1/tractors_by_popular' });
+    return res.data.data;
+  } catch (error) {
+    rejectWithValue(error);
+  }
+});
+
+const PopularTractorsSlice = createSlice({
+  name: 'populartractors',
+  initialState,
+  extraReducers: (builder) => {
+    builder.addCase(FetchPopularTractors.pending, (state) => {
+      state.status = 'pending'; state.loading = true; state.data = []; state.error = '';
+    });
+    builder.addCase(FetchPopularTractors.fulfilled, (state, action) => {
+      state.status = action.payload.length > 0 ? 'ok-exist' : 'ok-empty';
+      state.loading = false; state.data = action.payload; state.error = [];
+    });
+    builder.addCase(FetchPopularTractors.rejected, (state, action) => {
+      state.status = 'error'; state.loading = false; state.data = []; state.error = action.error;
+    });
+  },
+});
+
+export default PopularTractorsSlice.reducer;
